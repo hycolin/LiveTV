da30dde0485a5bc55984b3a8b27f3c922fa0da95
chenwang <chenwang@bixin.cn>
2022-09-08 11:23:47 +0800

    hello world

---
+++ LiveTV/LiveTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTVApp.swift
+//  LiveTV
+//
+//  Created by chenwang on 2022/9/8.
+//
+
+import SwiftUI
+
+@main
+struct LiveTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
